a1dc727d98a0c7cb030b65fd1e866a4c478227e7
David Gitahi <130385462+gitahidave@users.noreply.github.com>
2024-09-15 14:57:51 +0300

    Basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#Defining variables 'number1' and 'number2' by assigning them to the values '10' and '5' respectively
+number1 = 10
+number2 = 5
+
+#Calculating the sum of number1 and number2
+print("Addition of 10 and 5 is", number1 + number2)
+
+#Calculating the difference in number1 and number2
+print("Subtraction of 10 and 5 is", number1 - number2)
+
+#Calculating the product of number1 and number2
+print("Multiplication of 10 and 5 is", number1 * number2)
