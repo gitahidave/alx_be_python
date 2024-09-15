4772b08459244e6495ccf93fff8f08eb443c998c
David Gitahi <130385462+gitahidave@users.noreply.github.com>
2024-09-15 17:35:43 +0300

    Simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,16 @@
+principal = 1000
+#Value 1000 represents $1000
+
+rate = 0.05
+#Value 0.05 represents 5% annual interest
+
+time = 3
+#Value 3 represents 3 years
+
+#Calculating the simple interest using the 'I = P * R * T' formula,
+# where;
+#  'I' represents the interest
+#  'P' represents the principal
+#  'R' represents the rate
+#  'T' represents the time
+print("The simple interest is:", principal * rate * time)  
